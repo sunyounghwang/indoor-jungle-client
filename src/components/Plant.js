1215e2bd1394fdabc5bf578b0b29a31512a42e03
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-16 18:47:58 -0500

    Use react-bootstrap Card component

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const Plant = ({ plant }) => {
   const { img_url, name, type_of, location } = plant;
 
   return (
-    <div>
-      {name}
-    </div>
+    <Card className="plant-card">
+      <Card.Img variant="top" src={img_url}  />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Subtitle className="text-muted">{type_of}</Card.Subtitle>
+        <Card.Text className="m-2">
+          {location}
+        </Card.Text>
+        <Card.Link href="#">View More</Card.Link>
+      </Card.Body>
+    </Card>
   );
 }
 
