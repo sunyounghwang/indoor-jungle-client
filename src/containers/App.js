511485b8706811a3315305e80fa2e97acf79217f
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-16 17:27:02 -0500

    Create PlantsContainer component and add to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import PlantsContainer from './PlantsContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Indoor Jungle
-        </header>
+        <PlantsContainer />
       </div>
     );
   }
---
+++ src/containers/PlantsContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PlantsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Your Plant Collection
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default PlantsContainer;
