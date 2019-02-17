36f83d8b76719eccc2fd8fa926adef831f5aaac0
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-17 13:54:16 -0500

    Build PrimaryLayout and add to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import PlantsContainer from './PlantsContainer';
+import PrimaryLayout from '../components/PrimaryLayout';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PlantsContainer />
+      <div className="App container">
+        <PrimaryLayout />
       </div>
     );
   }
