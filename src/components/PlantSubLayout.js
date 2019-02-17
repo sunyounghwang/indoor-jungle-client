87cc74c97a8f7c6a60264d966d492c26ace8f84d
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-17 14:23:40 -0500

    Build PlantSubLayout and add a plants route to PrimaryLayout

---
+++ src/components/PlantSubLayout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlantsContainer from '../containers/PlantsContainer';
+
+const PlantSubLayout = () => (
+  <div className="plant-sub-layout">
+    Plants
+  </div>
+);
+
+export default PlantSubLayout;
--- src/components/PrimaryLayout.js
+++ src/components/PrimaryLayout.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from './Home';
+import PlantSubLayout from './PlantSubLayout';
 
 const PrimaryLayout = ({ match }) => (
   <div className="primary-layout">
     <NavBar />
     <main>
-      <Route exact path="/" render={Home} />
+      <Switch>
+        <Route exact path="/" render={Home} />
+        <Route path="/plants" render={PlantSubLayout} />
+        <Redirect to="/" />
+      </Switch>
     </main>
   </div>
 );
