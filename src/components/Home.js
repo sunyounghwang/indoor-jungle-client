fad14f75029a596d32d1f7bc904cf19b14d50bc5
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-17 14:07:31 -0500

    Remove unnecessary return from Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet, ei molestie detraxit pericula vis.
-        In vel aeque suscipit. Eos eu sumo agam dissentiunt.
-        In nec illud sensibus signiferumque, consul deserunt disputando te eum,
-        ut legimus accumsan facilisis vis. Eam nemore epicurei electram in,
-        wisi eligendi at nam.
-      </p>
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+    <p>
+      Lorem ipsum dolor sit amet, ei molestie detraxit pericula vis.
+      In vel aeque suscipit. Eos eu sumo agam dissentiunt.
+      In nec illud sensibus signiferumque, consul deserunt disputando te eum,
+      ut legimus accumsan facilisis vis. Eam nemore epicurei electram in,
+      wisi eligendi at nam.
+    </p>
+  </div>
+);
 
 export default Home;
