5f526edfba7e270cfbb4f72f971cb4b7bc3c9628
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-17 14:10:32 -0500

    Add a home route to PrimaryLayout

--- src/components/PrimaryLayout.js
+++ src/components/PrimaryLayout.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Route } from 'react-router-dom';
 import NavBar from './NavBar';
+import Home from './Home';
 
-const PrimaryLayout = () => (
+const PrimaryLayout = ({ match }) => (
   <div className="primary-layout">
     <NavBar />
+    <main>
+      <Route exact path="/" render={Home} />
+    </main>
   </div>
-)
+);
 
 export default PrimaryLayout;
