2c08a65fdb7e0ba74e1e390c02900dc66c024ea3
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-17 16:49:58 -0500

    Give form state and make inputs controlled components

--- src/containers/CreateForm.js
+++ src/containers/CreateForm.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 
 class CreateForm extends Component {
+  state = {
+    name: "",
+    type_of: "",
+    location: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="create-form">
@@ -8,9 +20,24 @@ class CreateForm extends Component {
 
         <form>
           <div className="form-group">
-            <input name="name" placeholder="Name" /><br />
-            <input name="type_of" placeholder="Type" /><br />
-            <input name="location" placeholder="Location in Home" />
+            <input
+              name="name"
+              placeholder="Name"
+              onChange={this.handleChange}
+              value={this.state.name}
+            /><br />
+            <input
+              name="type_of"
+              placeholder="Type"
+              onChange={this.handleChange}
+              value={this.state.type_of}
+            /><br />
+            <input
+              name="location"
+              placeholder="Location in Home"
+              onChange={this.handleChange}
+              value={this.state.location}
+            />
           </div>
           <input type="submit" value="Create Plant" className="btn btn-outline-primary" />
         </form>
