bd6036fd29812111862189237c99f24b5cf72eb2
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-19 09:50:48 -0500

    Create reducer and action creators for creating a plant

--- src/actions/plants.js
+++ src/actions/plants.js
@@ -1,4 +1,10 @@
-export const fetchPlantsSuccess = plants => ({ type: "FETCH_PLANTS_SUCCESS", plants });
+export const fetchPlantsSuccess = plants => ({
+  type: "FETCH_PLANTS_SUCCESS", plants
+});
+
+export const createPlantSuccess = plant => ({
+  type: "CREATE_PLANT_SUCCESS", plant
+});
 
 export const fetchPlants = () => {
   return dispatch => {
@@ -12,6 +18,31 @@ export const fetchPlants = () => {
       })
       .then(response => response.json())
       .then(plants => dispatch(fetchPlantsSuccess(plants)))
+      .catch(error => console.log('There was an error: ' + error.message));
+  };
+};
+
+export const createPlant = plant => {
+  const data = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(plant)
+  };
+
+  return dispatch => {
+    fetch(`http://localhost:3001/api/plants`, data)
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        return response;
+      })
+      .then(response => response.json())
+      .then(plant => dispatch(createPlantSuccess(plant)))
       .catch(error => console.log('There was an error: ' + error.message));
   };
 };
--- src/reducers/plants.js
+++ src/reducers/plants.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case "FETCH_PLANTS_SUCCESS":
       return action.plants;
+    case "CREATE_PLANT_SUCCESS":
+      return state.concat(action.plant);
     default:
       return state;
     }
