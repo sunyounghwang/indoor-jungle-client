7ff932dd214a12eb96635e5b46bb01e361b9f69e
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-20 17:40:53 -0500

    Set up default image and add styling to card

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -4,6 +4,9 @@ import Card from 'react-bootstrap/Card';
 const PlantCard = ({ plant, destroyPlant }) => {
   const { id, img_url, name, type_of, location } = plant;
 
+  let src = img_url;
+  if (!img_url) src = "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60";
+
   const handleClick = e => {
     e.preventDefault();
     destroyPlant(id);
@@ -11,7 +14,7 @@ const PlantCard = ({ plant, destroyPlant }) => {
 
   return (
     <Card className="plant-card">
-      <Card.Img variant="top" src={img_url}  />
+      <Card.Img variant="top" src={src}  />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle className="text-muted">{type_of}</Card.Subtitle>
