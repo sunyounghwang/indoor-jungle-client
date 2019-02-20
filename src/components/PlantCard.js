471f20d94d43d6b0876063fb7a238c6f26491362
sunyounghwang <sunyounghwang13@gmail.com>
2019-02-20 17:21:39 -0500

    Build handleClick function that invokes destroyPlant

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-const PlantCard = ({ plant }) => {
-  const { img_url, name, type_of, location } = plant;
+const PlantCard = ({ plant, destroyPlant }) => {
+  const { id, img_url, name, type_of, location } = plant;
+
+  const handleClick = e => {
+    e.preventDefault();
+    destroyPlant(id);
+  }
 
   return (
     <Card className="plant-card">
@@ -11,7 +16,7 @@ const PlantCard = ({ plant }) => {
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle className="text-muted">{type_of}</Card.Subtitle>
         <Card.Text className="m-2">{location}</Card.Text>
-        <Card.Link href="#">Delete</Card.Link>
+        <Card.Link href="#" onClick={handleClick}>Delete</Card.Link>
       </Card.Body>
     </Card>
   );
