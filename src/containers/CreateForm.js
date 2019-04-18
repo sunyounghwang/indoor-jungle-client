6a294978376b805fe14159d273f0c258e2a72692
sunyounghwang <sunyounghwang13@gmail.com>
2019-04-18 17:40:02 -0400

    Update form to use Formik

--- src/containers/CreateForm.js
+++ src/containers/CreateForm.js
@@ -1,72 +1,44 @@
 import React, { Component } from 'react';
+import { Formik, FormikProps, Form, Field } from 'formik';
 import { connect } from 'react-redux';
 import { createPlant } from '../actions/plants';
 
 class CreateForm extends Component {
-  state = {
-    name: "",
-    type_of: "",
-    location: "",
-    img_url: ""
-  };
-
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.createPlant(this.state);
-    this.setState({
-      name: "",
-      type_of: "",
-      location: "",
-      img_url: "",
-    });
+  handleSubmit = (values, { setSubmitting }) => {
+    this.props.createPlant(values);
     this.props.history.push('/plants');
-  };
+    setSubmitting(false);
+  }
 
   render() {
     return (
-      <div className="create-form">
-        <h1>Plant Details</h1>
-
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <input
-              name="name"
-              placeholder="Name"
-              onChange={this.handleChange}
-              value={this.state.name}
-            /><br />
-            <input
-              name="type_of"
-              placeholder="Type"
-              onChange={this.handleChange}
-              value={this.state.type_of}
-            /><br />
-            <input
-              name="location"
-              placeholder="Location in Home"
-              onChange={this.handleChange}
-              value={this.state.location}
-            />
-            <input
-              name="img_url"
-              placeholder="Image URL (optional)"
-              onChange={this.handleChange}
-              value={this.state.img_url}
-            />
-          </div>
-          <input
-            type="submit"
-            value="Create Plant"
-            className="btn btn-outline-primary"
-          />
-        </form>
-      </div>
+      <Formik
+        initialValues={{
+          name: "",
+          type_of: "",
+          location: "",
+          img_url: ""
+        }}
+        onSubmit={this.handleSubmit}
+        render={(formProps: FormikProps) => {
+          return (
+            <Form>
+              <div>
+                <Field name="name" placeholder="Name" />
+                <Field name="type_of" placeholder="Type" />
+                <Field name="location" placeholder="Location in Home" />
+                <Field name="img_url" placeholder="Image URL (optional)" />
+              </div>
+              <button
+                type="submit"
+                disabled={formProps.isSubmitting}
+                className="btn btn-outline-primary">
+                Add Plant
+              </button>
+            </Form>
+          );
+        }}
+      />
     );
   }
 }
