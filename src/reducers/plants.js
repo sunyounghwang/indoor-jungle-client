d01c56468e3c15ba0c24adea28013c886664a9f5
sunyounghwang <sunyounghwang13@gmail.com>
2019-04-20 15:50:01 -0400

    Create reducer for updating a plant

--- src/reducers/plants.js
+++ src/reducers/plants.js
@@ -4,6 +4,18 @@ export default (state = [], action) => {
       return action.plants;
     case "CREATE_PLANT_SUCCESS":
       return state.concat(action.plant);
+    case "UPDATE_PLANT_SUCCESS":
+      return state.map(plant => {
+        if (plant.id === action.plant.id) {
+          return Object.assign({}, plant, {
+            name: action.plant.name,
+            type_of: action.plant.type_of,
+            location: action.plant.location,
+            img_url: action.plant.img_url
+          });
+        }
+        return plant;
+      });
     case "DESTROY_PLANT_SUCCESS":
       return state.filter(plant => plant.id !== action.id);
     default:
